30d3ed465492f4fb1d666bf33f023eab49438f4a
Apteryx <apteryx.xyz@gmail.com>
2022-12-10 14:46:13 +1300

    fix(api): replace encryption separator

--- apps/api/src/handlers/Encryption.ts
+++ apps/api/src/handlers/Encryption.ts
@@ -15,12 +15,12 @@ export class Encryption extends null {
     public static hashString(data: string) {
         const salt = randomBytes(16).toString('hex');
         const hash = scryptSync(data, salt, 64).toString('hex');
-        return `${salt}:${hash}`;
+        return `${salt}-${hash}`;
     }
 
     /** Verifies that a string matches a hash, used for passwords. */
     public static verifyString(secret: string, input: string) {
-        const [salt, hash] = secret.split(':');
+        const [salt, hash] = secret.split('-');
         const hashBuffer = Buffer.from(hash, 'hex');
         const inputBuffer = scryptSync(input, salt, 64);
         return timingSafeEqual(hashBuffer, inputBuffer);
@@ -33,13 +33,13 @@ export class Encryption extends null {
         const cipher = createCipheriv('aes256', key, iv);
         const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
 
-        return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+        return `${iv.toString('hex')}-${encrypted.toString('hex')}`;
     }
 
     /** Generally decrypt a string. */
     public static decryptString(data: string) {
         const key = Buffer.from(CIPHER_KEY, 'hex');
-        const [iv, encrypted] = data.split(':');
+        const [iv, encrypted] = data.split('-');
         const decipher = createDecipheriv(
             'aes256',
             key,
