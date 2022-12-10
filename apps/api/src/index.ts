3b2d8ba1a317ba03442bd15b6fcb606bb252b870
Apteryx <apteryx.xyz@gmail.com>
2022-12-10 14:48:43 +1300

    fix(api): change port number

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,13 +2,20 @@ import 'dotenv/config';
 import 'dotenv-expand/config';
 import '@owenii/types';
 
+import process from 'node:process';
 import { Database } from '@owenii/database';
 import { Games } from '@owenii/games';
 import { Server } from './Server';
 
+let PORT = Number(process.env['PORT']);
+if (Number.isNaN(PORT)) {
+    console.warn('Missing or invalid PORT, defaulting to 3001');
+    PORT = 3_001;
+}
+
 const games = new Games();
 const database = new Database();
-const server = new Server(3_000);
+const server = new Server(PORT);
 
 void main();
 async function main() {
