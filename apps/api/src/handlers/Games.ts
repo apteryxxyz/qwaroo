03a411a36c28f85e4e7d8a4d1422cc8031899112
Apteryx <apteryx.xyz@gmail.com>
2022-12-12 09:03:58 +1300

    refactor(api): move to database `Game` models

--- apps/api/src/handlers/Games.ts
+++ apps/api/src/handlers/Games.ts
@@ -1,39 +1,34 @@
-import { Games as Modes } from '@owenii/games';
-import type { Game } from '@owenii/types';
-import { Validate } from '@owenii/validators';
-import fuzzysort = require('fuzzysort');
-import { shuffle } from 'shuffle-seed';
+import { Game } from '@owenii/database';
+import { createRegExp } from '@owenii/validators';
 import { APIError } from '#/utilities/APIError';
 
-const games = new Modes();
-
 export class Games extends null {
-    /** Get a filtered list of game categories. */
-    public static getCategories() {
-        const metas = games.getGames().map(game => game.meta);
-        const categories = metas.flatMap(meta => meta?.categories ?? []);
+    /** Get an existing user by their ID. */
+    public static async getCategories() {
+        const games = await Game.find({}).exec();
+        const categories = games.flatMap(game => game.categories ?? []);
         return Array.from(new Set(categories));
     }
 
-    /** Get a paginated list of games. */
-    public static async getPaginatedGames(term: string, page = 1) {
+    /** Get a list of users using pages. */
+    public static async getPaginatedGames(term?: string, page = 1) {
         if (typeof page !== 'number' || Number.isNaN(page))
             throw new APIError(422, 'Page must be a number');
         if (page < 1) throw new APIError(422, 'Page must be greater than 0');
 
-        const items = games
-            .getGames()
-            .map(game => game.meta)
-            .filter(Boolean) as Game.Meta<Game.Type>[];
-        const filteredGames = term
-            ? fuzzysort.go(term, items, { key: 'name' })
-            : items;
+        const query = Game.find(
+            term ? { title: createRegExp(term, false, 'i') } : {}
+        );
+        const total = await query.countDocuments().exec();
+        const users = await Game.find()
+            .merge(query)
+            .skip((page - 1) * 20)
+            .limit(20)
+            .exec();
 
-        const total = filteredGames.length;
-        const pageCount = Math.ceil(total / 25);
+        const pageCount = Math.ceil(total / 20);
         const previousPage = page > 1 ? page - 1 : null;
         const nextPage = page < pageCount ? page + 1 : null;
-        const currentItems = filteredGames.slice((page - 1) * 25, page * 25);
 
         return {
             previousPage,
@@ -41,56 +36,7 @@ export class Games extends null {
             nextPage,
             pageCount,
             itemCount: total,
-            items: currentItems,
-        };
-    }
-
-    /** Get the meta information for a game. */
-    public static getGameMeta(slug: string) {
-        const isValidSlug = Validate.Slug.test(slug);
-        if (!isValidSlug) throw new APIError(422, 'Game slug is invalid');
-
-        const game = games.getGames().find(game => game.slug === slug);
-        if (!game) throw new APIError(404, 'Game was not found');
-
-        return game.getMeta(games.basePath);
-    }
-
-    /** Get a paginated list of all the items for a game, shuffled with a seed. */
-    public static async getPaginatedGameItems(
-        slug: string,
-        seed: string,
-        page = 1
-    ) {
-        const isValidSlug = Validate.Slug.test(slug);
-        if (!isValidSlug) throw new APIError(422, 'Game slug is invalid');
-
-        if (typeof seed !== 'string')
-            throw new APIError(422, 'Seed must be a string');
-
-        if (typeof page !== 'number' || Number.isNaN(page))
-            throw new APIError(422, 'Page must be a number');
-        if (page < 1) throw new APIError(422, 'Page must be greater than 0');
-
-        const game = games.getGames().find(game => game.slug === slug);
-        if (!game) throw new APIError(404, 'Game was not found');
-
-        const items = await game.getItems(games.basePath);
-        const shuffledItems = shuffle(items, seed);
-
-        const total = shuffledItems.length;
-        const pageCount = Math.ceil(total / 10);
-        const previousPage = page > 1 ? page - 1 : null;
-        const nextPage = page < pageCount ? page + 1 : null;
-        const currentItems = shuffledItems.slice((page - 1) * 10, page * 10);
-
-        return {
-            previousPage,
-            currentPage: page,
-            nextPage,
-            pageCount,
-            itemCount: total,
-            items: currentItems,
+            items: users,
         };
     }
 }
