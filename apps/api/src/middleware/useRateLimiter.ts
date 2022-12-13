57146d586952c6e317759505871f9db96a5c6a18
Apteryx <apteryx.xyz@gmail.com>
2022-12-14 06:55:28 +1300

    fix(api): rate limiter

--- apps/api/src/middleware/useRateLimiter.ts
+++ apps/api/src/middleware/useRateLimiter.ts
@@ -2,20 +2,37 @@ import { ServerError as Error } from '@owenii/errors';
 import { RateLimiterMemory, type RateLimiterRes } from 'rate-limiter-flexible';
 import { handle } from '#/utilities/routeHandler';
 
-const rateLimiter = new RateLimiterMemory({ points: 6, duration: 1 });
+const rateLimiter = new RateLimiterMemory({ points: 60, duration: 60 });
 
 export function useRateLimiter() {
-    return handle((req, _, next) => {
+    return handle((req, res, next) => {
         const ip =
             req.header('X-Real-IP') ?? req.header('X-Forwarded-For') ?? req.ip;
 
+        const addHeaders = (rlr: RateLimiterRes, isError: boolean) => {
+            res.header('X-RateLimit-Limit', String(rateLimiter.points));
+            res.header('X-RateLimit-Remaining', String(rlr.remainingPoints));
+            res.header('X-RateLimit-Used', String(rlr.consumedPoints));
+            const resetAt = (Date.now() + rlr.msBeforeNext) / 1_000;
+            res.header('X-RateLimit-Reset', String(resetAt));
+
+            if (isError) {
+                const retryAfter = Math.ceil(rlr.msBeforeNext / 1_000);
+                res.header('Retry-After', String(retryAfter));
+            }
+        };
+
         rateLimiter
             .consume(ip)
-            .then(() => next())
+            .then(rlr => {
+                addHeaders(rlr, false);
+                next();
+            })
             .catch(error => {
-                const res = error as RateLimiterRes;
-                throw new Error(429, undefined, {
-                    'Retry-After': String(res.msBeforeNext / 1_000),
+                addHeaders(error as RateLimiterRes, true);
+                res.status(429).json({
+                    success: false,
+                    ...new Error(429).toJSON(),
                 });
             });
     });
