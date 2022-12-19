6495a48c485b794f34f5eb2bd0a3b7632ec4bef5
Apteryx <apteryx.xyz@gmail.com>
2022-12-19 18:55:22 +1300

    fix(web): build script failing in ci

--- apps/web/lib/hooks/useEnv.ts
+++ apps/web/lib/hooks/useEnv.ts
@@ -1,9 +1,19 @@
+function isCI() {
+    return process.env['CI'] === 'true';
+}
+
 export function useApiUrl() {
-    const apiUrl = String(process.env['NEXT_PUBLIC_API_URL'] ?? '');
+    // Environment variables are not available in the CI environment
+    // So we use localhost as a default
+    const apiUrl =
+        process.env['NEXT_PUBLIC_API_URL'] ??
+        (isCI() ? 'http://localhost' : '');
     return new URL(apiUrl);
 }
 
 export function useWebUrl() {
-    const webUrl = String(process.env['NEXT_PUBLIC_WEB_URL'] ?? '');
+    const webUrl =
+        process.env['NEXT_PUBLIC_WEB_URL'] ??
+        (isCI() ? 'http://localhost' : '');
     return new URL(webUrl);
 }
