68835f88dcd2ae7dc44276b629772aab5dc5ac16
Apteryx <apteryx.xyz@gmail.com>
2022-12-23 14:10:35 +1300

    fix(web): inability to exit login modal

--- apps/web/lib/utilities/backTo.ts
+++ apps/web/lib/utilities/backTo.ts
@@ -1,9 +1,11 @@
+import { useWebUrl } from '#/hooks/useEnv';
+
 export function setBackTo(url?: string) {
     const errors = document.querySelector('div[id^="error-"]');
     if (!url && errors) return;
 
     const backTo = url ?? window.location.href;
-    const backPath = new URL(backTo).pathname;
+    const backPath = new URL(backTo, useWebUrl()).pathname;
     localStorage.setItem('owenii.back_to', backPath);
 }
 
