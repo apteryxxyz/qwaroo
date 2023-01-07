a13e4717a8111dae0e4d4ddf6e12150066208ff6
Apteryx <apteryx.xyz@gmail.com>
2023-01-08 12:17:32 +1300

    fix(web): footer typo

--- apps/web/lib/components/FooterBar.tsx
+++ apps/web/lib/components/FooterBar.tsx
@@ -2,7 +2,7 @@ export function FooterBar() {
     return <footer id="footer-bar" className="z-10 bg-transparent">
         <div className="max-w-8xl w-full flex flex-row mx-auto">
             <span className="mx-auto text-xs">
-                Made love with ❤️ by Apteryx in 🇳🇿 New Zealand
+                Made with ❤️ by Apteryx in 🇳🇿 New Zealand
             </span>
         </div>
     </footer>;
