89a9c0f33cc0e594d5176d9d1ef705ffcf45a92e
Apteryx <apteryx.xyz@gmail.com>
2023-01-18 04:36:21 +1300

    feat(web): add privacy policy and terms of use

--- apps/web/lib/components/FooterBar.tsx
+++ apps/web/lib/components/FooterBar.tsx
@@ -1,9 +1,21 @@
+import { PlainButton } from './Input/PlainButton';
+
 export function FooterBar() {
     return <footer id="footer-bar" className="z-10 bg-transparent">
-        <div className="max-w-8xl w-full flex flex-row mx-auto">
-            <span className="mx-auto text-xs">
-                Made with ❤️ by Apteryx in 🇳🇿 New Zealand
-            </span>
+        <div className="max-w-8xl w-full flex flex-row mx-auto text-sm items-center justify-center">
+            <PlainButton
+                className="hover:opacity-80"
+                linkProps={{ href: '/policies/privacy' }}
+            >
+                Privacy Policy
+            </PlainButton>
+
+            <PlainButton
+                className="hover:opacity-80"
+                linkProps={{ href: '/policies/terms' }}
+            >
+                Terms of Use
+            </PlainButton>
         </div>
     </footer>;
 }
