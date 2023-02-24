c68c89995497ee7266754e8f61a012581cba0e21
Apteryx <apteryx.xyz@gmail.com>
2023-02-25 00:20:11 +1300

    feat(web): cache clients on server

--- apps/web/lib/contexts/Client.ts
+++ apps/web/lib/contexts/Client.ts
@@ -1,20 +1,25 @@
-import { Client } from '@qwaroo/client';
+import { Client, Collection } from '@qwaroo/client';
 import type { GetServerSidePropsContext } from 'next';
 import { createContext, useContext } from 'react';
-import { getApiUrl, getCdnUrl } from '#/utilities/getEnv';
+import { getApiUrl, getCdnUrl } from '#/utilities/env';
 
 export const ClientContext = createContext<Client>(null!);
 export const ClientProvider = ClientContext.Provider;
+export const ClientCache = new Collection<string, Client>();
 
 export function useClient(req?: GetServerSidePropsContext['req']) {
-    if (req) {
-        const userId = req.cookies['qwaroo.user_id'];
-        const token = req.cookies['qwaroo.token'];
+    if (!req) return useContext(ClientContext);
 
-        const client = createClient();
-        if (userId && token) client.prepare(userId, token);
-        return client;
-    } else return useContext(ClientContext);
+    const userId = req.cookies['qwaroo.user_id'];
+    const token = req.cookies['qwaroo.token'];
+
+    if (!userId || !token) return createClient();
+    if (ClientCache.has(token)) return ClientCache.get(token)!;
+
+    const client = createClient();
+    client.prepare(userId, token);
+    ClientCache.set(token, client);
+    return client;
 }
 
 export function createClient() {
