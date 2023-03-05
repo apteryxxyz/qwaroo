1d3cd82e55017a8aa8759084f2044105e7412832
Apteryx <apteryx.xyz@gmail.com>
2023-03-05 17:25:05 +1300

    fix(database): incorrect connection property

--- packages/database/lib/Database.ts
+++ packages/database/lib/Database.ts
@@ -2,18 +2,22 @@ import mongoose from 'mongoose';
 
 /** Database connection. */
 export class Database {
-    public connection!: typeof mongoose;
+    public mongoose!: typeof mongoose;
+
+    public get connection() {
+        return this.mongoose.connection;
+    }
 
     /** Connect to the MongoDB server. */
     public async connect(uri: string) {
-        this.connection = await mongoose.connect(uri);
+        this.mongoose = await mongoose.connect(uri);
         console.info('Connected to database');
     }
 
     /** Disconnect from the MongoDB server. */
     public async disconnect() {
-        if (this.connection) {
-            await this.connection.disconnect();
+        if (this.mongoose) {
+            await this.mongoose.disconnect();
             console.info('Disconnected from database');
         }
     }
