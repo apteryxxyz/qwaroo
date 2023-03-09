72f67535e0dc178d0de4e9f41a822cd31caca47d
Apteryx <apteryx.xyz@gmail.com>
2023-03-09 20:41:51 +1300

    fix(web): add v query to favicon urls

--- apps/web/lib/components/Seo/Seo.tsx
+++ apps/web/lib/components/Seo/Seo.tsx
@@ -14,20 +14,21 @@ export function Seo(props: Seo.Props) {
         <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/apple-touch-icon.png?v=2"
         />
         <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/favicon-32x32.png?v=2"
         />
         <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon-16x16.png?v=2"
         />
+        <link rel="icon" href="/favicon.ico?v=2" />
         <link rel="manifest" href="/site.webmanifest" />
 
         {props.children}
