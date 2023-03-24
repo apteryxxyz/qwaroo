13f686e05a9e1bb0842973151897b14af09d22b5
Apteryx <apteryx.xyz@gmail.com>
2023-03-25 00:07:13 +1300

    feat(discord): use `@maclary/lists`

--- bots/discord/src/index.ts
+++ bots/discord/src/index.ts
@@ -1,3 +1,4 @@
+import { Lists, Poster } from '@maclary/lists';
 import { Database } from '@qwaroo/database';
 import { getEnv } from '@qwaroo/server';
 import { ActivityType, Client, GatewayIntentBits, Partials } from 'discord.js';
@@ -6,6 +7,8 @@ import { GameManager } from '#/classes/GameManager';
 import './types';
 
 require('dotenv-expand').expand(require('dotenv').config());
+
+const isDevelopment = getEnv(String, 'NODE_ENV') === 'development';
 
 const database = new Database();
 container.database = database;
@@ -32,11 +35,31 @@ const maclary = new Maclary({
             : undefined,
 });
 
+const clientId = getEnv(String, 'DISCORD_APPLICATION_ID');
+
+const lists = [
+    new Lists.TopGG(clientId, getEnv(String, 'TOP_GG_KEY')),
+    new Lists.DiscordBots(clientId, getEnv(String, 'DISCORD_BOTS_KEY')),
+    // TODO: Add Botlist.me
+    new Lists.DiscordsCom(clientId, getEnv(String, 'DISCORDS_COM_KEY')),
+    new Lists.DiscordBotList(clientId, getEnv(String, 'DISCORD_BOT_LIST_KEY')),
+    new Lists.DiscordList(clientId, getEnv(String, 'DISCORD_LIST_KEY')),
+    new Lists.UniverseList(clientId, getEnv(String, 'UNIVERSE_LIST_KEY')),
+];
+const poster = new Poster(isDevelopment ? [] : lists, {
+    shardCount: () => client.shard?.count ?? 1,
+    guildCount: () => 11,
+    userCount: () => client.guilds.cache.reduce((a, b) => a + b.memberCount, 0),
+    voiceConnectionCount: () => 0,
+});
+container.poster = poster;
+
 void main();
 async function main() {
     Maclary.init(maclary, client);
     await database.connect(getEnv(String, 'MONGODB_URI'));
     await client.login(getEnv(String, 'DISCORD_TOKEN'));
+    poster.startAutoPost();
 }
 
 export { database, client, maclary };
--- bots/discord/src/types.ts
+++ bots/discord/src/types.ts
@@ -1,3 +1,4 @@
+import type { Poster } from '@maclary/lists';
 import type { Database } from '@qwaroo/database';
 import type { GameManager } from '#/classes/GameManager';
 
@@ -5,5 +6,6 @@ declare module 'maclary' {
     export interface Container {
         database: Database;
         games: GameManager;
+        poster: Poster;
     }
 }
