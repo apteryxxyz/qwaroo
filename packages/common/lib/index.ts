a312b0139d82a5a242ac9890a0aca34ddb145798
Apteryx <apteryx.xyz@gmail.com>
2023-03-28 14:51:46 +1300

    refactor(common): rename file

--- packages/common/lib/index.ts
+++ packages/common/lib/index.ts
@@ -8,6 +8,6 @@ export * from './errors/ClientError';
 export * from './errors/ServerError';
 
 // Utilities
-export * from './utilities/createRegExp';
+export * from './utilities/Validate';
 export * from './utilities/Slug';
 export * from './utilities/Query';
