9c067ef041f6dc54b62db3950ad1ef8391c67535
Apteryx <apteryx.xyz@gmail.com>
2023-05-13 06:59:24 +1200

    Fix games with lots of item data not saving due to express text limit

--- apps/cdn/src/index.ts
+++ apps/cdn/src/index.ts
@@ -7,7 +7,7 @@ const server = new Server();
 void main();
 async function main() {
     server.routers.push(require('./routes/fs').default());
-    server.app.use(require('express').text());
+    server.app.use(require('express').text({ limit: '50mb' }));
     server.setOnError((err, _req, res) => res.status(err.status).end());
 
     await server.listen(getEnv(Number, 'CDN_PORT'));
