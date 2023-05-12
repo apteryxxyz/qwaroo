1418750bf2cc771e1b0aa0514b302c9f161cdb00
Apteryx <apteryx.xyz@gmail.com>
2023-05-13 02:03:59 +1200

    Fix incorrect theme switch on reload

--- apps/web/lib/components/Header.tsx
+++ apps/web/lib/components/Header.tsx
@@ -1,16 +1,11 @@
-import { faCloudMoon } from '@fortawesome/free-solid-svg-icons/faCloudMoon';
-import { faCloudSun } from '@fortawesome/free-solid-svg-icons/faCloudSun';
 import { faGamepad } from '@fortawesome/free-solid-svg-icons/faGamepad';
-// import { faPenFancy } from '@fortawesome/free-solid-svg-icons/faPenFancy';
+import { faPenFancy } from '@fortawesome/free-solid-svg-icons/faPenFancy';
 import { faUserAlt } from '@fortawesome/free-solid-svg-icons/faUserAlt';
-import { useTheme } from 'next-themes';
 import { Container } from './Container';
 import { Button } from './Input/Button';
+import { ThemeSwitch } from './Input/ThemeSwitch';
 
 export function Header() {
-    const { theme, setTheme } = useTheme();
-    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
-
     return <Container
         parentType="header"
         parentClassName="fixed left-0 right-0 z-[2] shadow-xl bg-qwaroo-500 dark:bg-neutral-800"
@@ -40,24 +35,15 @@ export function Header() {
                     Profile
                 </Button>
 
-                {/* <Button
+                <Button
                     className="!bg-transparent"
                     iconProp={faPenFancy}
                     linkProps={{ href: '/games/create' }}
                 >
                     Create
-                </Button> */}
+                </Button>
 
-                <Button
-                    className="!bg-transparent"
-                    iconProp={
-                        (theme ?? 'light') === 'light'
-                            ? faCloudMoon
-                            : faCloudSun
-                    }
-                    ariaLabel="Toggle theme"
-                    onClick={() => toggleTheme()}
-                />
+                <ThemeSwitch />
             </div>
         </nav>
     </Container>;
