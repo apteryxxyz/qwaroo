0da12432975f87f5dd604e169c2e896eaebddee0
Apteryx <apteryx.xyz@gmail.com>
2023-05-16 04:42:44 +1200

    Fix source card responsiveness

--- apps/web/lib/components/Card/Card.tsx
+++ apps/web/lib/components/Card/Card.tsx
@@ -2,6 +2,7 @@ export function Card(props: Card.Props) {
     return <section
         className={`p-3 gap-3 rounded-xl shadow-md
         bg-neutral-100 dark:bg-neutral-800 ${props.className ?? ''}`}
+        onClick={props.onClick}
     >
         {props.children}
     </section>;
@@ -11,5 +12,6 @@ export namespace Card {
     export interface Props {
         className?: string;
         children: React.ReactNode | React.ReactNode[];
+        onClick?(): void;
     }
 }
--- apps/web/lib/components/Editor/Source.tsx
+++ apps/web/lib/components/Editor/Source.tsx
@@ -9,22 +9,54 @@ export function Source(props: Source.Props) {
         .filter(property => property.required)
         .map(prop => prop.name);
 
-    return <Card className={'flex ' + (props.onClick ? 'cursor-pointer' : '')}>
-        <Image
-            src={proxifyImageUrl(props.source.iconUrl)}
-            alt={`Icon for ${props.source.name}`}
-            width={100}
-            height={100}
-        />
+    return <Card
+        className={'flex ' + (props.onClick ? 'cursor-pointer' : '')}
+        onClick={props.onClick}
+    >
+        {/* Mobile card has icon and title inline */}
+        <div className="block md:hidden">
+            <div className="flex items-center justify-center gap-4">
+                <Image
+                    src={proxifyImageUrl(props.source.iconUrl)}
+                    alt={`Icon for ${props.source.name}`}
+                    width={100}
+                    height={100}
+                />
 
-        <div onClick={props.onClick} className="flex flex-col justify-center">
-            <h3 className="text-2xl font-bold">{props.source.name}</h3>
+                <h3 className="text-2xl font-bold h-full">
+                    {props.source.name}
+                </h3>
+            </div>
 
-            <p>{props.source.description}</p>
+            <div>
+                <p>{props.source.description}</p>
 
-            <small>
-                Requires the following inputs: {requiredProperties.join(', ')}
-            </small>
+                <small>
+                    Requires the following inputs:{' '}
+                    {requiredProperties.join(', ')}
+                </small>
+            </div>
+        </div>
+
+        {/* On desktop the icon is inline with a div containing the  */}
+        <div className="hidden md:flex gap-4">
+            <Image
+                src={proxifyImageUrl(props.source.iconUrl)}
+                alt={`Icon for ${props.source.name}`}
+                width={100}
+                height={100}
+            />
+
+            <div className="flex flex-col justify-center">
+                <h3 className="text-2xl font-bold">{props.source.name}</h3>
+
+                <p>{props.source.description}</p>
+
+                <small>
+                    Requires the following inputs:{' '}
+                    {requiredProperties.join(', ')}
+                </small>
+            </div>
         </div>
     </Card>;
 }
