2cd74a2e1237160e25f70a0b66e07b4b855e1ea8
Apteryx <apteryx.xyz@gmail.com>
2023-06-06 13:29:48 +1200

    Replace Docker with PM2

---
+++ pm2.staging.config.js
@@ -0,0 +1,12 @@
+const script = process.platform === 'win32'
+    ? '../../scripts/npm-windows.js' : 'npm';
+
+module.exports = [{
+    name: 'Qwaroo Staging',
+    cwd: './apps/www',
+    script,
+    args: 'run start',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+}];
